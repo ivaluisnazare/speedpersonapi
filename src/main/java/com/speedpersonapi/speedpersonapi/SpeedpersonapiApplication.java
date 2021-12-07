5c56520548159c157e6898cf0392e1805bf72d08
ivaluisnazare <ivaluisnazare@gmail.com>
2021-12-07 07:27:46 -0300

    first commit

---
+++ src/main/java/com/speedpersonapi/speedpersonapi/SpeedpersonapiApplication.java
@@ -0,0 +1,15 @@
+package com.speedpersonapi.speedpersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedpersonapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpeedpersonapiApplication.class, args);
+		System.out.printf("It's All Right!");
+	}
+
+}
